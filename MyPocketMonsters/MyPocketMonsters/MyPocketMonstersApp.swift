321e0f7b4e6426e72c20d63a46051ce91ecf8bc9
Prateek Kambadkone <prate_k@yahoo.com>
2024-06-29 22:32:04 +0200

    initialise project

---
+++ MyPocketMonsters/MyPocketMonsters/MyPocketMonstersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPocketMonstersApp.swift
+//  MyPocketMonsters
+//
+//  Created by Prateek on 2024/06/29.
+//
+
+import SwiftUI
+
+@main
+struct MyPocketMonstersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
